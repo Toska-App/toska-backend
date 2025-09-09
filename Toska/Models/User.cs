c78bce66041e5eb758abf363aa78728d508040fd
bita <nasrybita@gmail.com>
2025-09-09 19:49:37 +0330

    Add User Model for User Management

---
+++ Toska/Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace Toska.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+
+        //Identity info
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        //Contact info
+        public string Email { get; set; } = null!;
+
+
+        //Security
+        public string PasswordHash { get; set; } = null!;
+        public string Role { get; set; } = "User";
+
+
+        //Status
+        public bool IsActive { get; set; } = true;
+
+
+        //Audit
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+
+
+    }
+}
--- Toska/obj/Debug/net8.0/Toska.AssemblyInfo.cs
+++ Toska/obj/Debug/net8.0/Toska.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Toska")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59dd30af1e870963462f9e49b271d28bf14cefb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Toska")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Toska")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
